d4f79972e1c01a82b648f96daff8c90fba8d4b4a
AR Abdullah <tim@timrich.org>
2022-06-22 01:32:13 +0600

    Exercise Details part complete

---
+++ src/components/SimilarExercises.js
@@ -0,0 +1,17 @@
+import { Stack, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
+  return (
+    <Box>
+        <Typography>
+            Similar <span>Target Muscle</span> exercises
+        </Typography>
+        <Stack>
+            
+        </Stack>
+    </Box>
+  )
+}
+
+export default SimilarExercises
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,7 +1,32 @@
+import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Details from "../components/Details";
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+  useEffect(()=>{
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+    const fetchExerciseData = async () => {
+      const exerciseDburl = 'https://exercisedb.p.rapidapi.com';
+
+      const exerciseDetailData = await fetchData(`${exerciseDburl}/exercises/exercise/${id}`, exerciseOptions);
+
+      setExerciseDetail(exerciseDetailData)
+    }
+
+    fetchExerciseData()
+  },[id])
+
+  if(!exerciseDetail) return <p>No Data</p>;
+
   return (
-    <div>ExerciseDetail</div>
+    <Box sx={{ mt: {lg: '96px', xs: '60px' } }}>
+      <Details exerciseDetail={exerciseDetail} />
+      
+    </Box>
   )
 }
 
